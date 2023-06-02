ed905a56af92643752278f1ffa66c064003a1781
Valbs30 <lucasvalbusa30@gmail.com>
2023-06-02 08:12:22 -0300

    versão_1.0

---
+++ js/crud_despesas.js
@@ -0,0 +1,195 @@
+document.querySelector("#salvar").addEventListener("click", cadastrar_desp)
+
+let despesas = []
+let edit_desp = false
+
+// Carregar conteúdo
+window.addEventListener("load", () => {
+    despesas = JSON.parse(localStorage.getItem("despesas")) || []
+    atualizar_desp()
+})
+
+// Busca
+document.querySelector("#busca").addEventListener("keyup", () => {
+    let busca = document.querySelector("#busca").value.toLowerCase()
+    let despesasFiltradas = despesas.filter((despesa) => {
+      let nomeDesp = despesa.nome_desp.toLowerCase()
+      let dataDesp = formatarMonth(despesa.data_desp)
+      return nomeDesp.includes(busca) || dataDesp.includes(busca)
+    })
+    filtrar_desp(despesasFiltradas)
+})
+
+// Filtrar pela busca
+function filtrar_desp(despesas){
+    document.querySelector("#despesas").innerHTML = ""
+    despesas.forEach((despesa) => {
+        document.querySelector("#despesas").innerHTML += createCard_desp(despesa)
+    })
+}
+
+// Filtrar por campo
+function filtro_campo(campo, elemento, valor) {
+    document.querySelector(campo).addEventListener("change", () => {
+      let despesasFiltradas = despesas.filter(despesa => despesa[elemento] === valor)
+      filtrar_desp(despesasFiltradas)
+    })
+}
+
+filtro_campo("#filtro_pendente", "stpaga_desp", false)
+filtro_campo("#filtro_pago", "stpaga_desp", true)
+filtro_campo("#filtro_domestica", "cat_desp", "Doméstica")
+filtro_campo("#filtro_corporativa", "cat_desp", "Corporativa")
+filtro_campo("#filtro_outra", "cat_desp", "Outra")
+  
+document.querySelector("#limpa_filtros").addEventListener("change", () => {
+    filtrar_desp(despesas)
+})
+  
+// Atualizar
+function atualizar_desp(){
+    document.querySelector("#despesas").innerHTML = ""
+    localStorage.setItem("despesas", JSON.stringify(despesas))
+    despesas.forEach((despesa) => {
+        document.querySelector("#despesas").innerHTML += createCard_desp(despesa)
+    })
+}
+
+// Cadastrar
+function cadastrar_desp(){
+    let nome_desp = document.querySelector("#nome_desp").value
+    let data_desp = document.querySelector("#data_desp").value
+    let cons_desp = document.querySelector("#consumo_desp").value
+    let valor_desp = document.querySelector("#valor_desp").value
+    let cat_desp = document.querySelector("#categoria_desp").value
+    let desc_desp = document.querySelector("#descricao_desp").value
+    let id_desp = document.querySelector("#id_desp").value
+    let modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
+
+    cons_desp = parseFloat(cons_desp).toFixed(2)
+    valor_desp = parseFloat(valor_desp).toFixed(2)
+
+    let despesa = {
+        id_desp: Date.now(),
+        nome_desp,
+        data_desp,
+        cons_desp,
+        valor_desp,
+        cat_desp,
+        desc_desp,
+        stpaga_desp: false
+    }
+    
+    if (!validar_desp(despesa.nome_desp, document.querySelector("#nome_desp"))) return
+    if (!validar_desp(despesa.data_desp, document.querySelector("#data_desp"))) return
+    if (despesa.cat_desp == "Outra") {
+        if (!validar_desp(despesa.desc_desp, document.querySelector("#descricao_desp"))) return
+    }
+
+
+    if (edit_desp == true) {
+        let despesaEdita = despesas.find((despesa) => despesa.id_desp == id_desp)
+
+        if (despesaEdita) {
+            despesaEdita.nome_desp = document.querySelector("#nome_desp").value
+            despesaEdita.data_desp = document.querySelector("#data_desp").value
+            despesaEdita.cons_desp = document.querySelector("#consumo_desp").value
+            despesaEdita.valor_desp = document.querySelector("#valor_desp").value
+            despesaEdita.cat_desp = document.querySelector("#categoria_desp").value
+            despesaEdita.desc_desp = document.querySelector("#descricao_desp").value
+        }
+
+        edit_desp = false
+    }else{
+        despesas.push(despesa)
+    }
+
+    atualizar_desp()
+
+    modal.hide()
+}
+
+// Validação
+function validar_desp(valor, campo){
+    if(valor == ""){
+        campo.classList.add("is-invalid")
+        campo.classList.remove("is-valid")
+        return false
+    }
+
+    campo.classList.remove("is-invalid")
+    campo.classList.add("is-valid")
+    return true
+
+}
+
+// Formatar data
+function formatarMonth(valor) {
+    let monthFormatado = valor.substring(5) + "/" + valor.substring(0, 4)
+    return(monthFormatado)
+}
+
+// Editar
+function editar_desp(id_desp){
+    const despesa = despesas.find((despesa) => despesa.id_desp == id_desp)
+    
+    document.querySelector("#nome_desp").value = despesa.nome_desp
+    document.querySelector("#data_desp").value = despesa.data_desp
+    document.querySelector("#consumo_desp").value = despesa.cons_desp
+    document.querySelector("#valor_desp").value = despesa.valor_desp
+    document.querySelector("#categoria_desp").value = despesa.cat_desp
+    document.querySelector("#descricao_desp").value = despesa.desc_desp
+
+    edit_desp = true
+}
+
+// Apagar
+function apagar_desp(id_desp){
+    despesas = despesas.filter((despesa) => {
+        return despesa.id_desp != id_desp
+    })
+    atualizar_desp()
+}
+
+// Pagar
+function pagar_desp(id_desp){
+    let despesaEncontrado = despesas.find((despesa) => {
+        return despesa.id_desp == id_desp
+    })
+    despesaEncontrado.stpaga_desp = true
+    atualizar_desp()
+}
+
+// Criar card
+function createCard_desp(despesa){
+    let descricao = despesa.desc_desp ? `<p class="card-text">Descrição: ${despesa.desc_desp}</p>` : ""
+    let disabled = despesa.stpaga_desp ? "disabled" : ""
+    let status = despesa.stpaga_desp ? "Pago" : "Pendente"
+
+    return `
+        <div class="col-lg-3 col-md-6 col-12 mt-2">
+            <div class="card">
+                <div class="card-header bg-secondary-subtle d-flex justify-content-between">
+                    <h8>${despesa.nome_desp}</h8>
+                    <h8>data: ${formatarMonth(despesa.data_desp)}</h8>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Consumo: ${despesa.cons_desp} m³</p>
+                    <p class="card-text">Valor: R$${despesa.valor_desp}</p>
+                    <p class="card-text">Categoria: ${despesa.cat_desp}</p>
+                    ${descricao}
+                    <p class="card-text">Status: ${status}</p>
+                    <a onClick="pagar_desp(${despesa.id_desp})" href="#" class="btn btn-success ${disabled}" title="Marcar despesa paga">
+                        <i class="bi bi-check-lg"></i>
+                    </a>
+                    <a onClick="editar_desp(${despesa.id_desp})" href="#" class="btn btn-primary" title="Editar despesa" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <i class="bi bi-pencil"></i>
+                    </a>
+                    <a onClick="apagar_desp(${despesa.id_desp})" href="#" class="btn btn-danger" title="Apagar despesa">
+                        <i class="bi bi-trash"></i>
+                    </a>
+                </div> <!-- card-body -->
+            </div> <!-- card -->
+        </div> <!-- col -->
+    `
+}
---
+++ js/relatorio.js
@@ -0,0 +1,167 @@
+document.querySelector("#gerar_relatorio").addEventListener("click", verificar_dados)
+
+let relatorios = []
+
+window.addEventListener("load", () => {
+  relatorios = JSON.parse(localStorage.getItem("relatorios")) || []
+  atualizar_rel()
+})
+
+// Recuperar os dados do localStorage
+const despesasArmazenadas = JSON.parse(localStorage.getItem('despesas'))
+
+function atualizar_rel() {
+  document.querySelector("#relatorios").innerHTML = ""
+  localStorage.setItem("relatorios", JSON.stringify(relatorios))
+  relatorios.forEach((relatorio) => {
+    document.querySelector("#relatorios").innerHTML += gerar_relatorio(despesasArmazenadas, relatorio)
+  })
+}
+
+// Verificar se há dados no localStorage
+function verificar_dados() {
+  if (despesasArmazenadas) {
+    // Chamar a função fazer_relatorio com os dados do localStorage
+    let relatorio = calculos_relatorio(despesasArmazenadas)
+    relatorios.push(relatorio)
+    atualizar_rel()
+  }
+  else {
+    console.log('Não há despesas armazenadas no localStorage.')
+  }
+}
+function calculos_relatorio(despesasArmazenadas) {
+  let soma_valor = 0
+  let soma_consumo = 0
+  const num_despesas = despesasArmazenadas.length
+
+  // Calcular a soma total dos valores e consumos
+  for (let i = 0; i < num_despesas; i++) {
+    soma_valor += Number(despesasArmazenadas[i].valor_desp);
+    soma_consumo += Number(despesasArmazenadas[i].cons_desp);
+  }
+
+  const media_valor = (soma_valor / num_despesas).toFixed(2)
+  const media_consumo = (soma_consumo / num_despesas).toFixed(2)
+
+  // Gerar o relatório
+  const relatorio = {
+    id_relatorio: Date.now(),
+    total_valor: soma_valor,
+    total_consumo: soma_consumo,
+    media_valor: media_valor,
+    media_consumo: media_consumo,
+    num_despesas: num_despesas
+  }
+
+  return relatorio
+}
+
+function apagar_rel(id_relatorio) {
+  relatorios = relatorios.filter((relatorio) => {
+    return relatorio.id_relatorio != id_relatorio
+  })
+  atualizar_rel()
+}
+
+function formatarMonth(valor) {
+  let monthFormatado = valor.substring(5) + "/" + valor.substring(0, 4)
+  return (monthFormatado)
+}
+
+function obterDataHoraLocal() {
+  const dataHora = new Date()
+  const dataLocal = dataHora.toLocaleDateString()
+  const horaLocal = dataHora.toLocaleTimeString()
+
+  let datahora = dataLocal + " ás " + horaLocal
+  return datahora
+}
+
+function gerar_relatorio(despesasArmazenadas, relatorio) {
+  function criar_linhas_despesas() {
+    let linhas = ''
+    for (let i = 0; i < despesasArmazenadas.length; i++) {
+      linhas += `
+                <tr>
+                    <th scope="row">${despesasArmazenadas[i].nome_desp}</th>
+                    <td>${formatarMonth(despesasArmazenadas[i].data_desp)}</td>
+                    <td>${despesasArmazenadas[i].cons_desp}</td>
+                    <td>${despesasArmazenadas[i].valor_desp}</td>
+                    <td>${despesasArmazenadas[i].cat_desp}</td>
+                </tr>
+            `
+    }
+    return linhas
+  }
+
+  return `
+      <div id="estrutura_rel" class="bg-dark rounded m-3">
+        <div class="d-flex align-items-start">
+          <h1 class="text-white flex-grow-1 px-3 py-2">Relatório Consolidado de Consumos</h1>
+          <p class="text-white m-3">Relatório gerado no dia ${obterDataHoraLocal()}</p> 
+          <a onClick="apagar_rel(${relatorio.id_relatorio})" href="#" class="btn btn-danger m-3" title="Apagar relatorio">
+            <i class="bi bi-trash"></i>
+          </a>
+        </div>
+            <h2 class="text-white ps-3 pb-1">Visão Analítica</h2>
+            <div class="table-responsive px-3 d-flex">
+                <table class="table table-light table-striped table-hover table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">Despesa</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Consumo (m³)</th>
+                            <th scope="col">Valor (R$)</th>
+                            <th scope="col">Categoria</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        ${criar_linhas_despesas()}
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <th scope="row">Total (${relatorio.num_despesas} despesas)</th>
+                            <td>-</td>
+                            <td>${relatorio.total_consumo}</td>
+                            <td>${relatorio.total_valor}</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                    </tbody>
+                </table>
+            </div>
+
+            <h2 class="text-white ps-3 pb-1">Visão Consolidada</h2>
+            <div class="mx-3 gap-3">
+              <div class="table-responsive">
+                  <table class="table table-light table-striped table-hover table-sm">
+                      <thead>
+                          <tr>
+                              <th scope="row">Campo</th>
+                              <th scope="row">Valor</th>
+                          </tr>
+                      </thead>
+                      <tbody class="table-group-divider">
+                          <tr>
+                              <th scope="col">Consumo Total</th>
+                              <td>R$ ${relatorio.total_consumo}</td>
+                          </tr>
+                          <tr>
+                              <th scope="col">Média de Consumo</th>
+                              <td>R$ ${relatorio.media_consumo}</td>
+                          </tr>
+                          <tr>
+                              <th scope="col">Valor Total</th>
+                              <td>R$ ${relatorio.total_valor}</td>
+                          </tr>
+                          <tr>
+                              <th scope="col">Média de Valor</th>
+                              <td>R$ ${relatorio.media_valor}</td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+            </div>
+        </div>
+    `
+}
